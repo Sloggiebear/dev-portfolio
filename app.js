9998e9b48b033c8d25cb5e9c0f376197fd199a60
Logan McLain <Sloggiebear@users.noreply.github.com>
2021-11-28 19:43:04 +0000

    stripped laravel

--- app.js
+++ app.js
@@ -8,38 +8,38 @@ window.addEventListener('scroll', event => {
     (window.scrollY >= 50) ? button.classList.add('btn-outline-light') : button.classList.remove('btn-outline-light');
     (window.scrollY <= 50) ? button.classList.add('btn-dark') : button.classList.remove('btn-dark');
     (window.scrollY >= 50) ? clipper.classList.add('straight-clip') : clipper.classList.remove('straight-clip');
-
 })
 
-
 //Show logo text on mouseover
-var brandimage = document.getElementById("brand-image");
-var brandtext = document.getElementById("brand-text");
+let brandimage = document.getElementById('brand-image');
+let brandtext = document.getElementById("brand-text"); 
+console.log(brandimage);
+console.log(brandtext);
+
 
 brandimage.addEventListener('mouseover', event => {
     brandtext.classList.toggle('invisible');
+    
 })
 brandimage.addEventListener('mouseout', event => {
     brandtext.classList.toggle('invisible');
 })
 
-
-
 // Draw the wordpress logo when the section intersects
-const el = document.querySelector('#wordpress-drawing');
-
+const el = document.getElementById("wordpress-drawing");
+console.log(el);
 
 const options = {
     root: null,
     threshold: 0,
-    rootMargin: "-100px"
+    rootMargin: "100px"
 };
 
 const observer = new IntersectionObserver(function(entries, 
     observer) {
         entries.forEach (entry => {
             console.log("el", el);
-            entry.target.classList.add('draw-in');
+            entry.target.classList.toggle('draw-in');
         });
 }, options);
 
